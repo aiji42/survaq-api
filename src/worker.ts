a7858dc6705ecd9708c6a29c76546d9413749e97
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-05 21:04:43 +0900

    update for cors

--- src/worker.ts
+++ src/worker.ts
@@ -1,15 +1,24 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { Bindings } from "../bindings";
 import { makeSchedule } from "../libs/makeSchedule";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.use(
+  "/products/*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "OPTIONS"],
+    maxAge: 600,
+  })
+);
 app.get("/products/:id", async (c) => {
   const url = new URL(c.req.url);
   url.host = c.env.ORIGIN;
   const req = await fetch(new Request(url.toString(), c.req), {
     cf: {
-      cacheTtlByStatus: { "200-299": 1800, 404: 1, "500-599": 0 },
+      cacheTtlByStatus: { "200-299": 3600, 404: 1, "500-599": 0 },
       cacheEverything: true,
       cacheKey: c.req.url,
     },
