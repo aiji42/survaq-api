8a7d53e9410dd02325ad7cd3d3a6921cf59c5712
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-02-04 21:00:00 +0900

    リファクタ

--- src/routes/_apis/cms.ts
+++ src/routes/_apis/cms.ts
@@ -34,54 +34,51 @@ app.post("transaction-mail", async (c) => {
     const data = await getTransactionMail(key);
     if (!data) return;
 
-    let resource: typeof data.testResource = null;
-    let isTest = true;
-
-    if (data.status === "testPending" && data.testResource) {
-      resource = data.testResource;
-    } else if (data.status === "sendPending" && data.resource?.filename_disk) {
-      resource = data.resource;
-      isTest = false;
-    }
-
+    const isProd = data.status === "sendPending";
+    const resource =
+      data.status === "testPending"
+        ? data.testResource
+        : data.status === "sendPending"
+          ? data.resource
+          : null;
     if (!resource) return;
 
     try {
       const csvData = await env.CMS_BUCKETS.get(resource.filename_disk!);
       if (!csvData) throw new Error("csv file not found");
 
-      let text = await csvData.text();
-      // BOMを削除
-      const bom = "\uFEFF";
-      if (text.startsWith(bom)) text = text.slice(bom.length);
-
-      const records: Array<{ email: string; [key: string]: string }> = csvParse(text, {
-        columns: true,
-      });
-
+      const records = csvParse(removeBOM(await csvData.text()), { columns: true });
       const result = await sendTransactionMail(data, records);
       if (result.status !== 202) throw new Error(await result.text());
 
-      await updateTransactionMail(key, {
-        status: isTest ? "preparing" : "sent",
-        log: `[${new Date().toISOString()}] ${isTest ? "test " : ""}mail sent to ${records.length} addresses\n${data.log ?? ""}`,
-      });
-      if (!isTest) {
-        // 本番ならdirectusFileレコードを削除
-        await removeDirectusFiles(resource.id);
-        // R2からファイルを削除
-        await env.CMS_BUCKETS.delete(resource.filename_disk!);
-      }
+      const status = isProd ? "sent" : "preparing";
+      const log = `${isProd ? "" : "test "}mail sent to ${records.length} addresses`;
+      await updateTransactionMail(key, { status, log: appendLog(data.log, log) });
+
+      if (!isProd) return;
+
+      // 本番ならファイルを削除
+      await removeDirectusFiles(resource.id);
+      await env.CMS_BUCKETS.delete(resource.filename_disk!);
     } catch (e) {
-      if (e instanceof Error)
-        await updateTransactionMail(key, {
-          status: "failed",
-          log: `[${new Date().toISOString()}] ${isTest ? "test " : ""}mail sending failed: ${e.message}\n${data.log ?? ""}`,
-        });
+      const log = `${isProd ? "" : "test "}mail sending failed: ${e instanceof Error ? e.message : String(e)}`;
+      await updateTransactionMail(key, { status: "failed", log: appendLog(data.log, log) });
     }
   });
 
   return c.text("webhook received");
 });
+
+const removeBOM = (text: string) => {
+  const bom = "\uFEFF";
+  if (text.startsWith(bom)) return text.slice(bom.length);
+  return text;
+};
+
+const appendLog = (orgLog: string | null, newLog: string) => {
+  const split = (orgLog ?? "").split("\n");
+  split.unshift(`[${new Date().toISOString()}] ${newLog}`);
+  return split.join("\n");
+};
 
 export default app;
