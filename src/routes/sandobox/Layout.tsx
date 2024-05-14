1e49995a008ba67b5f17f03d289c37b11fd5f9ee
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-05-14 14:49:25 +0900

    sandboxのリソースエントリーポイントを本番と開発で分ける

--- src/routes/sandobox/Layout.tsx
+++ src/routes/sandobox/Layout.tsx
@@ -1,12 +1,20 @@
 import type { Child, FC } from "hono/jsx";
 
-export const Layout: FC<{ lang?: "ja" | "en"; children: Child }> = ({ children, lang = "ja" }) => {
+export const Layout: FC<{ lang?: "ja" | "en"; isDev?: boolean; children: Child }> = ({
+  children,
+  lang = "ja",
+  isDev = false,
+}) => {
   return (
     <html lang={lang}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>sandbox</title>
-        <script type="module" src="/src/web-components/index.ts" />
+        {!isDev ? (
+          <script type="module" src="/static/web-components.js" />
+        ) : (
+          <script type="module" src="/src/web-components/index.ts" />
+        )}
       </head>
       <body>{children}</body>
     </html>
--- src/routes/sandobox/index.tsx
+++ src/routes/sandobox/index.tsx
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
 import { Layout } from "./Layout";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: { DEV?: string } }>();
 
 app.get("/delivery-schedules", (c) => {
   return c.html(
-    <Layout>
+    <Layout isDev={!!c.env.DEV}>
+      <survaq-delivery-schedule productId="8719571812557" />
+      <survaq-delivery-schedule productId="8719571812557" delayedOnly />
+      <survaq-delivery-schedule productId="7266231288013" />
+      <survaq-delivery-schedule productId="7266231288013" delayedOnly />
+      <survaq-delivery-schedule productId="8719571812557a" />
+    </Layout>,
+  );
+});
+
+app.get("/delivery-schedules", (c) => {
+  return c.html(
+    <Layout isDev={!!c.env.DEV}>
       <survaq-delivery-schedule productId="8719571812557" />
       <survaq-delivery-schedule productId="8719571812557" delayedOnly />
       <survaq-delivery-schedule productId="7266231288013" />
