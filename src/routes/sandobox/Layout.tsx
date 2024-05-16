1d2b53fad199ec115b6fc176dd1ccb912dc4043c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-05-16 12:20:03 +0900

    ビルドステップにcssのビルドも含める

--- src/routes/sandobox/Layout.tsx
+++ src/routes/sandobox/Layout.tsx
@@ -11,11 +11,14 @@ export const Layout: FC<{ lang?: "ja" | "en"; isDev?: boolean; children: Child }
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>sandbox</title>
         {!isDev ? (
-          <script type="module" src="/static/web-components.js" />
+          <>
+            <script type="module" src="/static/web-components.js" />
+            <link type="text/css" rel="stylesheet" href="/static/style.css" />
+          </>
         ) : (
           <>
             <script type="module" src="/src/web-components/index.ts" />
-            <link type="text/css" rel="stylesheet" href="/src/web-components/globals.css" />
+            <link type="text/css" rel="stylesheet" href="/src/globals.css" />
           </>
         )}
       </head>
